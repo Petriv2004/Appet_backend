4bd12b3f67da61438b0c094278b6b90b834a641b
Felipe Triviño <141465004+Petriv2004@users.noreply.github.com>
2025-02-24 14:45:20 -0500

    Delete appet/appet directory

--- appet/appet/src/main/java/com/example/appet/repositories/MascotaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.appet.repositories;
-
-import com.example.appet.models.Mascota;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MascotaRepository extends JpaRepository<Mascota, Integer> {
-}
