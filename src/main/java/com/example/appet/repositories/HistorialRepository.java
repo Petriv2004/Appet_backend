04263773379fdf62a24d541dda4f83d0c860c927
Felipe Triviño <141465004+Petriv2004@users.noreply.github.com>
2025-02-27 21:04:36 -0500

    Delete src directory

--- src/main/java/com/example/appet/repositories/HistorialRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.appet.repositories;
-
-import com.example.appet.models.Historial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistorialRepository extends JpaRepository<Historial, Integer> {
-}
