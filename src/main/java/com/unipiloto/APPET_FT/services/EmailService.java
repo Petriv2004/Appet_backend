2be9f0aa633297b27d6631ea13b115f0a3d09b56
Felipe Triviño <141465004+Petriv2004@users.noreply.github.com>
2025-03-20 17:33:20 -0500

    Add files via upload

---
+++ src/main/java/com/unipiloto/APPET_FT/services/EmailService.java
@@ -0,0 +1,24 @@
+package com.unipiloto.APPET_FT.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void enviarCorreo(String destinatario, String asunto, String mensaje) {
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(destinatario);
+        email.setSubject(asunto);
+        email.setText(mensaje);
+        mailSender.send(email);
+    }
+}
