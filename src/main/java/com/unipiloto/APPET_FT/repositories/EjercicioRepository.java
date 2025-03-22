f96f68dd7904108ccfe2777007f7ba004b4625d4
Felipe Triviño <141465004+Petriv2004@users.noreply.github.com>
2025-03-22 12:57:48 -0500

    Delete src directory

--- src/main/java/com/unipiloto/APPET_FT/repositories/EjercicioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unipiloto.APPET_FT.repositories;
-
-import com.unipiloto.APPET_FT.models.Ejercicio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EjercicioRepository extends JpaRepository<Ejercicio,Integer> {
-}
