87334b66c75445009eb6fec36a28136ce4c9bc2e
Felipe Triviño <141465004+Petriv2004@users.noreply.github.com>
2025-03-27 20:18:45 -0500

    Delete src directory

--- src/main/java/com/unipiloto/APPET_FT/repositories/HistorialRepository.java
+++
@@ -1,8 +0,0 @@
-package com.unipiloto.APPET_FT.repositories;
-
-import com.unipiloto.APPET_FT.models.Historial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistorialRepository extends JpaRepository<Historial, Integer> {
-
-}
