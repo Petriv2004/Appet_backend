413ef1df10bf20a8519a1de1297cd2fe5023649c
Petriv2004 <felipetrivinogarzon@gmail.com>
2025-04-10 20:47:36 -0500

    fix error correo y hacer recorrido

--- src/main/java/com/unipiloto/APPET_FT/services/EmailService.java
+++ src/main/java/com/unipiloto/APPET_FT/services/EmailService.java
@@ -3,6 +3,7 @@ package com.unipiloto.APPET_FT.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,7 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
+    @Async
     public void enviarCorreo(String destinatario, String asunto, String mensaje) {
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(destinatario);
